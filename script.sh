f4126af60fc30c163a0329d4ba8d8d3a63da1507
QuickZam <111820344+QuickZam@users.noreply.github.com>
2022-11-21 16:45:37 +0000

    dfd

--- script.sh
+++ script.sh
@@ -2,6 +2,7 @@ echo "Creating deployment with services"
 kubectl create -f redis-deployment.yaml
 kubectl create -f flask-application-deployment.yaml
 kubectl create -f celery-worker-deployment.yaml
-kubectl create -f flower-deployment.yaml 
+
 echo "List of deployments and service"
 kubectl get deployments,svc 
+kubectl create -f flower-deployment.yaml 
